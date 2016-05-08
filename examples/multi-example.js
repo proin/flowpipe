a7d40767b01b8cd6bed32a1b3346adbda913d478
proin <proin@proinlab.com>
2016-05-08 20:32:31 +0900

    [0.3.4] multiple usage

---
+++ examples/multi-example.js
@@ -0,0 +1,149 @@
+var flowpipe1 = require('../index');
+
+flowpipe1
+    .init(function (next) {
+        // init before start
+        var page = 1;
+        next(null, page);
+    })
+    .pipe('start', function (next, page) {
+        // page is that previous next function's variable
+        setTimeout(function (err) {
+            // next is function that proceed next pipe, parallel or loopback
+            next(err, page);
+        }, 1000);
+    })
+    .pipe('list', function (next, page) {
+        console.log('start page: ' + page);
+        // preparing list for parallel
+        var list = [{no: 1}, {no: 2}, {no: 3}];
+        for (var i = 0; i < list.length; i++) {
+            list[i].page = page;
+            list[i].delay = 3 - i; // delay time in parallel process
+        }
+        // next before parallel, pass only second parameter to parallel process.
+        // second parameter must be array.
+        // other parameters pass to next pipe or loopback.
+        next(null, list, page);
+    })
+    .parallel('parallel process', function (next, data) {
+        // data is indicating each of list items.
+        setTimeout(function () {
+            data.title = 'title-' + data.no;
+            next(null, data);
+        }, data.delay * 1000);
+    })
+    .parallel('parallel process 2', function (next, data) {
+        // data is indicating each of list items.
+        setTimeout(function () {
+            data.title = 'title-' + data.no;
+            next(null, data);
+        }, data.delay * 1000);
+    })
+    .pipe('pass-1', function (next, parallel, page) {
+        console.log('pass-1');
+        next(null, parallel, page);
+    })
+    .pipe('pass-else', function (next, parallel, page) {
+        console.log('pass-else');
+        next(null, parallel, page);
+    })
+    .loopback('pipe-list', function (loop, next, instance, parallel, page) {
+        // loopback(process_type-process_name, fn)
+        // - process_type: pipe, parallel
+        // - process_name: must defined
+        // - fn(loop, next, instance, others...)
+        //   - loop(err, variables...): passing variable to target process
+        //   - next(err, variables...): proceeding if loop ended
+        //   - instance: maintainable variable in loop, object type {}
+        //   - others: passed from before process
+        if (!instance.data) instance.data = [];
+        for (var i = 0; i < parallel.length; i++)
+            instance.data.push(parallel[i]);
+        if (page < 5) loop(null, page + 1);
+        else next(null, instance.data);
+    })
+    .pipe('finalize', function (next, data) {
+        next(null, data);
+    })
+    .end(function (err, test) {
+        // end(callback) or end()
+        // this must be declared, if not all function don't working.
+        // proceed in the end or occured error in process
+    })
+    .graph('./basic-example-graph-2.html');
+
+var flowpipe2 = require('../index');
+
+flowpipe2
+    .init(function (next) {
+        // init before start
+        var page = 1;
+        next(null, page);
+    })
+    .pipe('start', function (next, page) {
+        // page is that previous next function's variable
+        setTimeout(function (err) {
+            // next is function that proceed next pipe, parallel or loopback
+            next(err, page);
+        }, 1000);
+    })
+    .pipe('list', function (next, page) {
+        console.log('start page: ' + page);
+        // preparing list for parallel
+        var list = [{no: 1}, {no: 2}, {no: 3}];
+        for (var i = 0; i < list.length; i++) {
+            list[i].page = page;
+            list[i].delay = 3 - i; // delay time in parallel process
+        }
+        // next before parallel, pass only second parameter to parallel process.
+        // second parameter must be array.
+        // other parameters pass to next pipe or loopback.
+        next(null, list, page);
+    })
+    .parallel('parallel process', function (next, data) {
+        // data is indicating each of list items.
+        setTimeout(function () {
+            data.title = 'title-' + data.no;
+            next(null, data);
+        }, data.delay * 1000);
+    })
+    .parallel('parallel process 2', function (next, data) {
+        // data is indicating each of list items.
+        setTimeout(function () {
+            data.title = 'title-' + data.no;
+            next(null, data);
+        }, data.delay * 1000);
+    })
+    .pipe('pass-1', function (next, parallel, page) {
+        console.log('pass-1');
+        next(null, parallel, page);
+    })
+    .pipe('pass-else', function (next, parallel, page) {
+        console.log('pass-else');
+        next(null, parallel, page);
+    })
+    .loopback('pipe-list', function (loop, next, instance, parallel, page) {
+        // loopback(process_type-process_name, fn)
+        // - process_type: pipe, parallel
+        // - process_name: must defined
+        // - fn(loop, next, instance, others...)
+        //   - loop(err, variables...): passing variable to target process
+        //   - next(err, variables...): proceeding if loop ended
+        //   - instance: maintainable variable in loop, object type {}
+        //   - others: passed from before process
+        if (!instance.data) instance.data = [];
+        for (var i = 0; i < parallel.length; i++)
+            instance.data.push(parallel[i]);
+        if (page < 5) loop(null, page + 1);
+        else next(null, instance.data);
+    })
+    .pipe('finalize', function (next, data) {
+        next(null, data);
+    })
+    .end(function (err, test) {
+        // end(callback) or end()
+        // this must be declared, if not all function don't working.
+        // proceed in the end or occured error in process
+    })
+    .graph('./basic-example-graph-1.html');
